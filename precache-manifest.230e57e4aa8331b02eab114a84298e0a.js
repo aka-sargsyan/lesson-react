57b885450be8eab93cecf43228d8d96ab32758ea
aka-sargsyan <akasargsyan87@gmail.com>
2020-10-05 23:03:01 +0400

    Updates

---
+++ precache-manifest.230e57e4aa8331b02eab114a84298e0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1000d21d211173ba578b7fbcb3f6c8",
+    "url": "/lesson-react/index.html"
+  },
+  {
+    "revision": "8871948f11ff92b2b9a4",
+    "url": "/lesson-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4f54ef1c1a5bd5afa90a",
+    "url": "/lesson-react/static/js/2.79cdc384.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lesson-react/static/js/2.79cdc384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8871948f11ff92b2b9a4",
+    "url": "/lesson-react/static/js/main.21e75909.chunk.js"
+  },
+  {
+    "revision": "cd9ed1a9a2ec73a998a7",
+    "url": "/lesson-react/static/js/runtime-main.627b2b49.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lesson-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
