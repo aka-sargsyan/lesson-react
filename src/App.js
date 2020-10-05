bf870dfae565f6eca086378ec69730b59cd948da
aka-sargsyan <akasargsyan87@gmail.com>
2020-10-05 23:18:58 +0400

    added word

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React good!
+          Learn React every time, it is good
         </a>
       </header>
     </div>
